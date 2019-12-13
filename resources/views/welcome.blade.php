d625f4027d7b845687deadd63a11cb1cd9b5d245
unknown <ikhawahyuramadhan@gmail.com>
2019-12-13 16:31:30 +0700

    initial update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,8 +8,10 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
         <!-- Styles -->
+        <link rel="stylesheet" href="{{asset('css/ecomStyle.css')}}">
+        <link rel="stylesheet" href="{{asset('css/ecomStyleRespons.css')}}">
 
     </head>
     <body>
@@ -42,8 +44,73 @@
                 <p class="section-center-description text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore recusandae reprehenderit hic deleniti fugiat, quibusdam aliquid illum reiciendis? Fugit expedita iusto ratione esse nostrum! Nihil pariatur illum omnis excepturi voluptatem.</p>
                 
                 <div class="text-center button-container"></div>
+                <a href="#" class="button">Featured</a>
+                <a href="#" class="button">On Sale</a>
+                
+                <div class="product text-center">
+                    <div class="product">
+                        <a href="#"><img src="img/Asus Zenbook.png" alt="product Asus Zenbook"></a>
+                        <a href="#"><div class="product-name">Asus Zenbook</div></a>
+                        <div class="product-price">Rp. 15.000.000</div>
+                    </div>
+                    <div class="product">
+                        <a href="#"><img src="img/Asus Zenbook.png" alt="product Asus Zenbook"></a>
+                        <a href="#"><div class="product-name">Asus Zenbook</div></a>
+                        <div class="product-price">Rp. 15.000.000</div>
+                    </div>
+                    <div class="product">
+                        <a href="#"><img src="img/Asus Zenbook.png" alt="product Asus Zenbook"></a>
+                        <a href="#"><div class="product-name">Asus Zenbook</div></a>
+                        <div class="product-price">Rp. 15.000.000</div>
+                    </div>
+                    <div class="product">
+                        <a href="#"><img src="img/Asus Zenbook.png" alt="product Asus Zenbook"></a>
+                        <a href="#"><div class="product-name">Asus Zenbook</div></a>
+                        <div class="product-price">Rp. 15.000.000</div>
+                    </div>
+                    <div class="product">
+                        <a href="#"><img src="img/Asus Zenbook.png" alt="product Asus Zenbook"></a>
+                        <a href="#"><div class="product-name">Asus Zenbook</div></a>
+                        <div class="product-price">Rp. 15.000.000</div>
+                    </div>
+                    <div class="product">
+                        <a href="#"><img src="img/Asus Zenbook.png" alt="product Asus Zenbook"></a>
+                        <a href="#"><div class="product-name">Asus Zenbook</div></a>
+                        <div class="product-price">Rp. 15.000.000</div>
+                    </div>
+                    <div class="product">
+                        <a href="#"><img src="img/Asus Zenbook.png" alt="product Asus Zenbook"></a>
+                        <a href="#"><div class="product-name">Asus Zenbook</div></a>
+                        <div class="product-price">Rp. 15.000.000</div>
+                    </div>
+                    <div class="product">
+                        <a href="#"><img src="img/Asus Zenbook.png" alt="product Asus Zenbook"></a>
+                        <a href="#"><div class="product-name">Asus Zenbook</div></a>
+                        <div class="product-price">Rp. 15.000.000</div>
+                    </div>
+                    {{-- </div> end product --}}
+                
+                    <div class="text-center button-container">
+                        <a href="#" class="button">View more product</a>
+                    </div>
+            </div>  
+            {{-- end container  --}}
+        </div> 
+        {{-- end feature section --}}
+
+        <div class="blog-section">
+            <div class="container">
+                <h1 class="text-center">From Our Blog</h1>
+                <p class="section-description text-center">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, quibusdam?</p>
+
+                <div class="blog-posts">
+                    <div class="blog-post" id="blog1">
+
+                    </div>
+                </div>
             </div>
         </div>
+
     </body>
 </html>
   
