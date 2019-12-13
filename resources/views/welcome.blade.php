57d988004a5c9f12dd3422b3f1a1d1696323b49b
unknown <ikhawahyuramadhan@gmail.com>
2019-12-13 14:55:44 +0700

    initial commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,97 +4,48 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Ecommerce</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
+       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
         <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
 
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+        <div class="top-nav container">
+            <div class="logo">Ecommerce</div>
+                <ul>
+                    <li><a href="#">Shop</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Blog</a></li>
+                    <li><a href="#">Cart</a></li>
+                </ul>
+            {{-- end top-nav  --}}
+        </div>
+        <div class="asus container"></div>
+        <h1>Ecommerce</h1>
+        <p>Just try to build an example website Ecommerce</p>
+        <div class="hero-button">
+            <a href="#" class="button button-white">button 1</a>
+            <a href="#" class="button button-white">button 2</a>
+        </div>
+        <div class="asus-image">
+            <img src="img/Asus Zenbook.png" alt="Asus Zenbook">
+        </div>
+        {{-- end asus Zenbook --}}
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
+        <div class="featured-section">
+            <div class="container">
+                <h1 class="text-center"> Ecommerce</h1>
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                <p class="section-center-description text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore recusandae reprehenderit hic deleniti fugiat, quibusdam aliquid illum reiciendis? Fugit expedita iusto ratione esse nostrum! Nihil pariatur illum omnis excepturi voluptatem.</p>
+                
+                <div class="text-center button-container"></div>
             </div>
         </div>
     </body>
 </html>
+  
+
+
